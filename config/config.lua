468841f07a72df9ce761ff28c30309e29643a4e6
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-16 16:49:55 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -5,9 +5,11 @@ Config = {}
 	Config.ClientClear = 264 --default 264 (Q)
 	
 	--clear the chat server sided
-	Config.EnableServerClear = false --change to true to allow admins to clear the chat for everyone
+	Config.EnableServerClear = true --change to true to allow admins to clear the chat for everyone
 	Config.ServerUnlock = 19 --the same as above for client clear
 	Config.ServerClear = 269 --default 269 (S)
 	Config.NoPermissions = "You have no permissions to use this!" --message for people trying to use server side chat clear.
 	Config.UseMythicNotify = false --change to true to use Mythoc Notify as message system. You will need to install this: https://forum.fivem.net/t/dev-resource-mythic-notifications/587071 to use it.
 	
+	--chat author
+	Config.Author = "SYSTEM"
