7fc185c8b68ec1219c8e05cf5e865508583d5a3f
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-16 12:47:53 +0200

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -5,7 +5,7 @@ Config = {}
 	Config.ClientClear = 264 --default 264 (Q)
 	
 	--clear the chat server sided
-	Config.EnableServerClear = true --change to true to allow admins to clear the chat for everyone
+	Config.EnableServerClear = false --change to true to allow admins to clear the chat for everyone
 	Config.ServerUnlock = 19 --the same as above for client clear
 	Config.ServerClear = 269 --default 269 (S)
 	Config.NoPermissions = "You have no permissions to use this!" --message for people trying to use server side chat clear.
