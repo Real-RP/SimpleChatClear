4ef1d5e44336aa62cad8fbb962d88b15e87647c1
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-25 17:43:58 +0200

    added config part for hb_notifications
    added config parts

--- config/config.lua
+++ config/config.lua
@@ -9,7 +9,8 @@ Config = {}
 	Config.ServerUnlock = 19 --the same as above for client clear
 	Config.ServerClear = 269 --default 269 (S)
 	Config.NoPermissions = "You have no permissions to use this!" --message for people trying to use server side chat clear.
-	Config.UseMythicNotify = false --change to true to use Mythoc Notify as message system. You will need to install this: https://forum.fivem.net/t/dev-resource-mythic-notifications/587071 to use it.
-	
+	Config.UseMythicNotify = false --change to true to use Mythic Notify as message system. You will need to install this: https://forum.fivem.net/t/dev-resource-mythic-notifications/587071 to use it.
+	Config.UsehbNotify = false --change to true to use hb_notificatipns as message system. You will need to install this: https://github.com/Real-RP/hb_notifications to use it.
+
 	--chat author
 	Config.Author = "SYSTEM"
