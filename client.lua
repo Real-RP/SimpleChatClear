cdf3218fd3a128197a1e051fe6b125f3fcb236d9
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-25 17:41:54 +0200

    Update client.lua
    added the possibility to display the global chat clear message via hb_notifications

--- client.lua
+++ client.lua
@@ -27,6 +27,8 @@ RegisterNetEvent("NoPermissions")
 AddEventHandler("NoPermissions", function()
 	if Config.UseMythicNotify == true then
 		exports['mythic_notify']:DoHudText('error', Config.NoPermissions)
+	elseif Config.UsehbNotify == true then
+		TriggerEvent("hb_notifications:display", "top-middle", Config.NoPermissions, "rgb(252, 3, 3)" , 5000)
 	else
 		TriggerEvent('chat:addMessage', { args = { "^8^*"..Config.Author, Config.NoPermissions }, color = 0,0,0 })
 	end
