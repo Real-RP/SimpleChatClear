bff964473f8cbb771fafead23ec44c6df279363d
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-25 17:51:11 +0200

    added the needed hb_notifications config options

--- config/config.lua
+++ config/config.lua
@@ -9,6 +9,7 @@ Config = {}
 	Config.ServerUnlock = 19 --the same as above for client clear
 	Config.ServerClear = 269 --default 269 (S)
 	Config.NoPermissions = "You have no permissions to use this!" --message for people trying to use server side chat clear.
+	Config.Error_configs = "This Error appears because of the fact that you have set both message settings to true... please fix this as soon as possible."
 	Config.UseMythicNotify = false --change to true to use Mythic Notify as message system. You will need to install this: https://forum.fivem.net/t/dev-resource-mythic-notifications/587071 to use it.
 	Config.UsehbNotify = false --change to true to use hb_notificatipns as message system. You will need to install this: https://github.com/Real-RP/hb_notifications to use it.
 
