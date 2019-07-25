9cdd4f65a3ba8ae20a7d33ba8ac084ec0f27ff6a
Real-RP <47250861+Real-RP@users.noreply.github.com>
2019-07-25 18:15:25 +0200

    fixing stuff

--- server.lua
+++ server.lua
@@ -10,6 +10,7 @@ RegisterServerEvent("getPermissions")
 AddEventHandler("getPermissions", function(_source)
 	if IsPlayerAceAllowed(_source, "ServerChatClear.cmds") then
 		TriggerClientEvent("chat:clear", -1)
+		TriggerClientEvent("MessageAfterServerClear", _source)
 	elseif not IsPlayerAceAllowed(_source, "ServerChatClear.cmds") then
 		TriggerClientEvent("NoPermissions", _source)
 	end
